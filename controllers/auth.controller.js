3db2476091689a398592713614b2668f3d096db9
CodersTokyo <phucpv62@wru.vn>
2021-03-09 19:24:36 +0700

    bai 16

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('../db');
+var md5 = require('md5');
 
 module.exports.login = (req, res) => {
 	res.render('auth/login')
@@ -22,7 +23,9 @@ module.exports.postLogin = (req, res) => {
 		return;
 	}
 
-	if (user.password !== password) {
+	hashedPassword = md5(password)
+	console.log(hashedPassword)
+	if (user.password !== hashedPassword) {
 		res.render('auth/login', {
 			errors: [
 			'Wrong password'
